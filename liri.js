53f258077c6ade3a48b379df7aca01bae49df67a
Fermenting <realferalxanderyell@gmail.com>
2018-12-09 23:26:16 -0800

    preliminary file structure

---
+++ liri.js
@@ -0,0 +1,17 @@
+// Language Interpretation & Recognition Interface
+
+//Search Avenues
+  //Spotify for songs
+  //Bands in Town for concerts
+  //OMDB for Movies KEY 187ccf02
+
+  require("dotenv").config();
+
+  //Add code required to import 'keys.js' and store in variable
+//var spotify = new Spotify(keys.spotify);
+
+//commands
+  //concert-this
+  //spotify-this-song
+  //movie-this
+  //do-what-it-says
